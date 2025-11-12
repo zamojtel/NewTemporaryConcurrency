ec2fcc1c387499d55614ce992e6ec46180ff4659
Bartek Zamojtel <56871145+zamojtel@users.noreply.github.com>
2025-11-12 15:03:02 +0100

    Update solution

--- solution.rs
+++ solution.rs
@@ -1,10 +1,11 @@
 use rustls::pki_types::pem::PemObject;
-use rustls::{ClientConnection,pki_types::CertificateDer, RootCertStore, ServerConnection, StreamOwned, pki_types::ServerName};
+use rustls::{ClientConnection, RootCertStore,pki_types::ServerName};
+use rustls::{ServerConfig, ServerConnection, StreamOwned};
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use std::io::{Read, Write};
 use std::sync::Arc;
 use hmac::{Hmac, Mac};
 use sha2::Sha256;
-use crate::certs;
 
 // You can add here other imports from std or crates listed in Cargo.toml.
 
@@ -23,6 +24,8 @@ pub struct SecureClient<L: Read + Write> {
 pub struct SecureServer<L: Read + Write> {
     // Add here any fields you need.
     phantom: PhantomData<L>,
+    hmac_key: Vec<u8>,
+    stream: StreamOwned<ServerConnection, L>,
 }
 
 impl<L: Read + Write> SecureClient<L> {
@@ -72,7 +75,7 @@ impl<L: Read + Write> SecureClient<L> {
 
         payload.extend_from_slice(&len);
         payload.extend_from_slice(&data);
-        payload.extend_from_slice(&hmac_tag);
+        payload.extend_from_slice(hmac_tag.as_ref());
 
         self.stream.write_all(&payload).unwrap();
     }
@@ -91,24 +94,27 @@ impl<L: Read + Write> SecureServer<L> {
         server_private_key: &str,
         server_full_chain: &str,
     ) -> Self {
-        let certs: Vec<CertificateDer<'static>> = CertificateDer::from_pem_slice(
-            server_full_chain.as_bytes()
-        ).into_iter().map(|c| c.into_owned()).collect();
+        // cert chain z PEM
+        let certs: Vec<CertificateDer<'static>> =
+        CertificateDer::pem_slice_iter(server_full_chain.as_bytes())
+            .flatten()
+            .map(|c| c.into_owned())   // to jest OK dla CertificateDer
+            .collect();
 
-        let mut keys = rustls::pki_types::PrivateKeyDer::from_pem_slice(server_private_key.as_bytes())
-            .expect("invalid private key PEM");
-        let key = keys.remove(0);
+        let key: PrivateKeyDer<'_> =
+            PrivateKeyDer::from_pem_slice(server_private_key.as_bytes())
+                .expect("invalid private key PEM");
 
-        let server_cfg = rustls::ServerConfig::builder()
+        let cfg = ServerConfig::builder()
             .with_no_client_auth()
             .with_single_cert(certs, key)
             .expect("bad cert or key");
 
-        let conn = ServerConnection::new(Arc::new(server_cfg)).unwrap();
+        let conn = ServerConnection::new(Arc::new(cfg)).unwrap();
         let stream = StreamOwned::new(conn, link);
 
         SecureServer {
-            phantom: PhantomData,
+            phantom: std::marker::PhantomData,
             hmac_key: hmac_key.to_vec(),
             stream,
         }
@@ -118,7 +124,22 @@ impl<L: Read + Write> SecureServer<L> {
     /// (i.e., without the message size and without the HMAC tag) if the
     /// message's HMAC tag is correct. Otherwise, returns `SecureServerError`.
     pub fn recv_message(&mut self) -> Result<Vec<u8>, SecureServerError> {
-        unimplemented!()
+
+        let mut len_buf = [0u8; 4];
+        self.stream.read_exact(&mut len_buf).unwrap();
+        let len = u32::from_be_bytes(len_buf) as usize;
+
+        let mut data = vec![0u8; len];
+        self.stream.read_exact(&mut data).unwrap();
+
+        let mut tag = [0u8; 32];
+        self.stream.read_exact(&mut tag).unwrap();
+
+        let mut mac = HmacSha256::new_from_slice(&self.hmac_key).unwrap();
+        mac.update(&data);
+        mac.verify_slice(&tag).map_err(|_| SecureServerError::InvalidHmac)?;
+
+        Ok(data)
     }
 }
 
